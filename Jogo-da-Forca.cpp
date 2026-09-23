5f2f8ae3301691502b0004d34c6ea1774d28ace0
Rafael Taffo Montanha <rafael.taffo07@gmail.com>
2026-09-23 20:09:39 -0300

    feat: adiciona as funções menuJogo() e forcaIA()
    a função forcaIA() adiciona uma lista pequena de palavras a serem sorteadas.

--- Jogo-da-Forca.cpp
+++ Jogo-da-Forca.cpp
@@ -5,6 +5,8 @@
 
 void limparTela();
 int menuPrincipal();
+int menuJogo();
+void forcaIA();
 
 int main(void)
 {
@@ -26,4 +28,24 @@ int menuPrincipal()
     printf("3 - Sair\n\n");
     printf("Opcao: ");
 	scanf(" %d", &opcao);
+}
+
+int menuJogo()
+{
+	int opcao;
+	printf("|1 - Adivinhar uma letra|\n");
+    printf("|2 - Adivinhar a palavra|\n\n");
+    printf("Opcao: ");
+	scanf(" %d", &opcao);
+}
+
+void forcaIA()
+{
+	char *palavras[] = { "Bola", "Girassol", "Laranja", "Computador", "Paralelepipedo", "Guitarra" };
+    int total = 6;
+	
+    srand(time(NULL)); 
+    int indice = rand() % total;
+	
+    strcpy(palavra_secreta, palavras[indice]);
 }
