5878965116670ab54a50f8355c57df61e434b4b5
Rafael Taffo Montanha <rafael.taffo07@gmail.com>
2026-09-23 20:04:42 -0300

    feat: adiciona as funções limparTela() e menuPrincipal()

--- Jogo-da-Forca.cpp
+++ Jogo-da-Forca.cpp
@@ -3,8 +3,27 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+void limparTela();
+int menuPrincipal();
+
 int main(void)
 {
-	
+	int opcao;
+	opcao = menuPrincipal();
 }
 	
+void limparTela()
+{
+	system("cls");
+}
+
+int menuPrincipal()
+{
+	int opcao;
+	printf("========================= Jogo da Forca =========================\n\n");
+    printf("1 - Iniciar Novo Jogo (IA)\n");
+    printf("2 - Iniciar Novo Jogo (2 Jogadores)\n");
+    printf("3 - Sair\n\n");
+    printf("Opcao: ");
+	scanf(" %d", &opcao);
+}
